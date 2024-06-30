4745ea920db35b1089a00cde65a422bc51822e3d
faizahkurratulaini22 <macanasianews@gmail.com>
2024-06-30 14:38:44 +0700

    menambahkan 2 model

--- komen/models.py
+++ komen/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from posting.models import Posting
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Posting, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment on {self.post.title}'
